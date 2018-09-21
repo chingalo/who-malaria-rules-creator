2256a5a05782b9f090445fb637cbe205ed0618ce
Joseph Chingalo <profschingalo@gmail.com>
2018-09-21 09:22:09 +0300

    Changes to be committed:  modified: 	 config/data-items.js 	modified:   index.js

--- config/data-items.js
+++ config/data-items.js
@@ -1,13 +1,170 @@
 module.exports = [{
+        "id": "OCN59f0jJbk",
+        "dimensionItem": "OCN59f0jJbk",
+        "displayName": "Proportion of foci investigated",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "kRasaq1REFp",
+        "dimensionItem": "kRasaq1REFp",
+        "displayName": "Number of total malaria cases (confirmed + presumed)",
+        "dimensionItemType": "DATA_ELEMENT"
+    }, {
+        "id": "q5EPiDt5Xzt",
+        "dimensionItem": "q5EPiDt5Xzt",
+        "displayName": "Malaria inpatient reporting completeness (%)",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "YS1Rf9k88HX",
+        "dimensionItem": "YS1Rf9k88HX",
+        "displayName": "Malaria deaths reporting completeness (%)",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "GxlrIgMyEf4",
+        "dimensionItem": "GxlrIgMyEf4",
+        "displayName": "Number of malaria deaths",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "af7VIQU9Gsb",
+        "dimensionItem": "af7VIQU9Gsb",
+        "displayName": "Non-malaria inpatient deaths",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "UHoPVgnALcd",
+        "dimensionItem": "UHoPVgnALcd",
+        "displayName": "Non-malaria outpatient cases",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "nrJvyLYwsHA",
+        "dimensionItem": "nrJvyLYwsHA",
+        "displayName": "Proportion of foci classified",
+        "dimensionItemType": "INDICATOR"
+    }, {
         "id": "v0WZQQ6gKAX",
         "dimensionItem": "v0WZQQ6gKAX",
         "displayName": "Number of inpatient malaria cases",
+        "dimensionItemType": "DATA_ELEMENT"
+    }, {
+        "id": "DPNsg3wjxGm",
+        "dimensionItem": "DPNsg3wjxGm",
+        "displayName": "Number of malaria cases tested with microscopy",
+        "dimensionItemType": "DATA_ELEMENT"
+    }, {
+        "id": "HMUc4RajgaC",
+        "dimensionItem": "HMUc4RajgaC",
+        "displayName": "Number of malaria cases classified as local",
+        "dimensionItemType": "DATA_ELEMENT"
+    }, {
+        "id": "Te6QHrjYc8k",
+        "dimensionItem": "Te6QHrjYc8k",
+        "displayName": "Slide positivity rate (microscopy)",
+        "dimensionItemType": "INDICATOR"
+    }, {
+        "id": "EM5cpI2NZ7d",
+        "dimensionItem": "EM5cpI2NZ7d",
+        "displayName": "Percentage of P. falciparum",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "clAcoaqWA5q",
+        "dimensionItem": "clAcoaqWA5q",
+        "displayName": "Percentage of P. vivax",
+        "dimensionItemType": "INDICATOR"
+    }, {
+        "id": "gTPP0wYkXBA",
+        "dimensionItem": "gTPP0wYkXBA",
+        "displayName": "Total malaria confirmed cases",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "fj29hAOSNDk",
+        "dimensionItem": "fj29hAOSNDk",
+        "displayName": "Annual blood examination rate",
+        "dimensionItemType": "INDICATOR"
+    }, {
+        "id": "lZi1cjtvVB6",
+        "dimensionItem": "lZi1cjtvVB6",
+        "displayName": "Number of new active foci with the current calendar year",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "gTPP0wYkXBA",
+        "dimensionItem": "gTPP0wYkXBA",
+        "displayName": "Total malaria confirmed cases",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "HfEGwjpq3Rq",
+        "dimensionItem": "HfEGwjpq3Rq",
+        "displayName": "Presumed cases",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "IPI8Xmc1nnT",
+        "dimensionItem": "IPI8Xmc1nnT",
+        "displayName": "Test positivity rate (RDT)",
+        "dimensionItemType": "INDICATOR"
+    }, {
+        "id": "lWrQhvx4YUA",
+        "dimensionItem": "lWrQhvx4YUA",
+        "displayName": "Proportion of cases investigated",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "c9o7AH6loTf",
+        "dimensionItem": "c9o7AH6loTf",
+        "displayName": "Proportion of cases classified",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "pmRn0GsUfr6",
+        "dimensionItem": "pmRn0GsUfr6",
+        "displayName": "Number of malaria cases tested with RDT",
         "dimensionItemType": "DATA_ELEMENT"
     },
     {
         "id": "FDgw48IMC7A",
         "dimensionItem": "FDgw48IMC7A",
         "displayName": "Non-malaria inpatient cases",
+        "dimensionItemType": "INDICATOR"
+    }, {
+        "id": "c9o7AH6loTf",
+        "dimensionItem": "c9o7AH6loTf",
+        "displayName": "Proportion of cases classified",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "MljjlLCWohp",
+        "dimensionItem": "MljjlLCWohp",
+        "displayName": "Proportion of local cases",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "OCN59f0jJbk",
+        "dimensionItem": "OCN59f0jJbk",
+        "displayName": "Proportion of foci investigated",
+        "dimensionItemType": "INDICATOR"
+    }, {
+        "id": "ySyleCjszMH",
+        "dimensionItem": "ySyleCjszMH",
+        "displayName": "Proportion of outpatient malaria cases",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "UROz5lfu4ab",
+        "dimensionItem": "UROz5lfu4ab",
+        "displayName": "Proportion of inpatient malaria",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "ploh4JSyBCf",
+        "dimensionItem": "ploh4JSyBCf",
+        "displayName": "Proportion of malaria deaths",
         "dimensionItemType": "INDICATOR"
     }
 ]
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const {
   headers,
   serverAddress
 } = require("./config/config");
+const _ = require('lodash');
 
 const dataItems = require('./config/data-items');
 
@@ -15,14 +16,14 @@ async function main() {
   try {
     const indicators = await indicatorHelper.getAllIndicators(headers, serverAddress);
     const dataElements = await dataElementHelper.getAllDataElements(headers, serverAddress);
-    const rules = await functionRulesHelper.getFunctionRules(indicators, dataElements, dataItems);
+    const rules = await functionRulesHelper.getFunctionRules(indicators, dataElements, _.unionBy(dataItems, 'id'));
     // @todo handling updating rules with default ones, rule.json
     const functionPayload = { ...await functionRulesHelper.getFunctionFromServer(headers, serverAddress),
       rules: rules
-    }
+    };
     await functionRulesHelper.updateFunction(headers, serverAddress, functionPayload);
-
+    console.log("Thumbs up!!");
   } catch (exception) {
-    console.log(exception)
+    console.log("exception ", exception);
   }
 }
