71ac677129a250cbbb9a94b988ddd599cacbb8d2
Joseph Chingalo <profschingalo@gmail.com>
2018-10-08 17:39:28 +0300

    Update itesm for rules creations

--- config/data-items.js
+++ config/data-items.js
@@ -5,41 +5,6 @@ module.exports = [{
         "dimensionItemType": "INDICATOR"
     },
     {
-        "id": "kRasaq1REFp",
-        "dimensionItem": "kRasaq1REFp",
-        "displayName": "Number of total malaria cases (confirmed + presumed)",
-        "dimensionItemType": "DATA_ELEMENT"
-    }, {
-        "id": "q5EPiDt5Xzt",
-        "dimensionItem": "q5EPiDt5Xzt",
-        "displayName": "Malaria inpatient reporting completeness (%)",
-        "dimensionItemType": "INDICATOR"
-    },
-    {
-        "id": "YS1Rf9k88HX",
-        "dimensionItem": "YS1Rf9k88HX",
-        "displayName": "Malaria deaths reporting completeness (%)",
-        "dimensionItemType": "INDICATOR"
-    },
-    {
-        "id": "GxlrIgMyEf4",
-        "dimensionItem": "GxlrIgMyEf4",
-        "displayName": "Number of malaria deaths",
-        "dimensionItemType": "DATA_ELEMENT"
-    },
-    {
-        "id": "af7VIQU9Gsb",
-        "dimensionItem": "af7VIQU9Gsb",
-        "displayName": "Non-malaria inpatient deaths",
-        "dimensionItemType": "INDICATOR"
-    },
-    {
-        "id": "UHoPVgnALcd",
-        "dimensionItem": "UHoPVgnALcd",
-        "displayName": "Non-malaria outpatient cases",
-        "dimensionItemType": "INDICATOR"
-    },
-    {
         "id": "nrJvyLYwsHA",
         "dimensionItem": "nrJvyLYwsHA",
         "displayName": "Proportion of foci classified",
@@ -48,83 +13,6 @@ module.exports = [{
         "id": "v0WZQQ6gKAX",
         "dimensionItem": "v0WZQQ6gKAX",
         "displayName": "Number of inpatient malaria cases",
-        "dimensionItemType": "DATA_ELEMENT"
-    }, {
-        "id": "DPNsg3wjxGm",
-        "dimensionItem": "DPNsg3wjxGm",
-        "displayName": "Number of malaria cases tested with microscopy",
-        "dimensionItemType": "DATA_ELEMENT"
-    }, {
-        "id": "HMUc4RajgaC",
-        "dimensionItem": "HMUc4RajgaC",
-        "displayName": "Number of malaria cases classified as local",
-        "dimensionItemType": "DATA_ELEMENT"
-    }, {
-        "id": "Te6QHrjYc8k",
-        "dimensionItem": "Te6QHrjYc8k",
-        "displayName": "Slide positivity rate (microscopy)",
-        "dimensionItemType": "INDICATOR"
-    }, {
-        "id": "EM5cpI2NZ7d",
-        "dimensionItem": "EM5cpI2NZ7d",
-        "displayName": "Percentage of P. falciparum",
-        "dimensionItemType": "INDICATOR"
-    },
-    {
-        "id": "clAcoaqWA5q",
-        "dimensionItem": "clAcoaqWA5q",
-        "displayName": "Percentage of P. vivax",
-        "dimensionItemType": "INDICATOR"
-    }, {
-        "id": "gTPP0wYkXBA",
-        "dimensionItem": "gTPP0wYkXBA",
-        "displayName": "Total malaria confirmed cases",
-        "dimensionItemType": "INDICATOR"
-    },
-    {
-        "id": "fj29hAOSNDk",
-        "dimensionItem": "fj29hAOSNDk",
-        "displayName": "Annual blood examination rate",
-        "dimensionItemType": "INDICATOR"
-    }, {
-        "id": "lZi1cjtvVB6",
-        "dimensionItem": "lZi1cjtvVB6",
-        "displayName": "Number of new active foci with the current calendar year",
-        "dimensionItemType": "DATA_ELEMENT"
-    },
-    {
-        "id": "gTPP0wYkXBA",
-        "dimensionItem": "gTPP0wYkXBA",
-        "displayName": "Total malaria confirmed cases",
-        "dimensionItemType": "INDICATOR"
-    },
-    {
-        "id": "HfEGwjpq3Rq",
-        "dimensionItem": "HfEGwjpq3Rq",
-        "displayName": "Presumed cases",
-        "dimensionItemType": "INDICATOR"
-    },
-    {
-        "id": "IPI8Xmc1nnT",
-        "dimensionItem": "IPI8Xmc1nnT",
-        "displayName": "Test positivity rate (RDT)",
-        "dimensionItemType": "INDICATOR"
-    }, {
-        "id": "lWrQhvx4YUA",
-        "dimensionItem": "lWrQhvx4YUA",
-        "displayName": "Proportion of cases investigated",
-        "dimensionItemType": "INDICATOR"
-    },
-    {
-        "id": "c9o7AH6loTf",
-        "dimensionItem": "c9o7AH6loTf",
-        "displayName": "Proportion of cases classified",
-        "dimensionItemType": "INDICATOR"
-    },
-    {
-        "id": "pmRn0GsUfr6",
-        "dimensionItem": "pmRn0GsUfr6",
-        "displayName": "Number of malaria cases tested with RDT",
         "dimensionItemType": "DATA_ELEMENT"
     },
     {
@@ -165,6 +53,399 @@ module.exports = [{
         "id": "ploh4JSyBCf",
         "dimensionItem": "ploh4JSyBCf",
         "displayName": "Proportion of malaria deaths",
+        "dimensionItemType": "INDICATOR"
+    }, {
+        "id": "DPNsg3wjxGm",
+        "dimensionItem": "DPNsg3wjxGm",
+        "displayName": "Number of malaria cases tested with microscopy",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "pmRn0GsUfr6",
+        "dimensionItem": "pmRn0GsUfr6",
+        "displayName": "Number of malaria cases tested with RDT",
+        "dimensionItemType": "DATA_ELEMENT"
+    }, {
+        "id": "Te6QHrjYc8k",
+        "dimensionItem": "Te6QHrjYc8k",
+        "displayName": "Slide positivity rate (microscopy)",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "IPI8Xmc1nnT",
+        "dimensionItem": "IPI8Xmc1nnT",
+        "displayName": "Test positivity rate (RDT)",
+        "dimensionItemType": "INDICATOR"
+    }, {
+        "id": "ZCCrHrs1Ta2",
+        "dimensionItem": "ZCCrHrs1Ta2",
+        "displayName": "Proportion of population at risk",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "lz1FE0wvo1m",
+        "dimensionItem": "lz1FE0wvo1m",
+        "displayName": "Number of malaria foci classified as active (with ongoing transmission, locally acquired case(s) detected within the current transmission calendar year )",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "Qzfvt1YRb1Y",
+        "dimensionItem": "Qzfvt1YRb1Y",
+        "displayName": "Number of malaria foci classified as residual non-active (Transmission interrupted recently, 1-3 years ago)",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "LH9FKWl1FfP",
+        "dimensionItem": "LH9FKWl1FfP",
+        "displayName": "Number of malaria Foci classified as cleared-up (no local transmission for >3 years, no longer considered residual non-active)",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "gTPP0wYkXBA",
+        "dimensionItem": "gTPP0wYkXBA",
+        "displayName": "Total malaria confirmed cases",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "DYpakDmCvNG",
+        "dimensionItem": "DYpakDmCvNG",
+        "displayName": "Number of malaria cases classified as indigenous",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "Ppc51eaCPHb",
+        "dimensionItem": "Ppc51eaCPHb",
+        "displayName": "Number of malaria cases investigated",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "nx6lSAN1YmB",
+        "dimensionItem": "nx6lSAN1YmB",
+        "displayName": "Proportion of cases investigated within N2 days of diagnosis",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "EM5cpI2NZ7d",
+        "dimensionItem": "EM5cpI2NZ7d",
+        "displayName": "Percentage of P. falciparum",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "clAcoaqWA5q",
+        "dimensionItem": "clAcoaqWA5q",
+        "displayName": "Percentage of P. vivax",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "lZi1cjtvVB6",
+        "dimensionItem": "lZi1cjtvVB6",
+        "displayName": "Number of new active foci with the current calendar year",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "EHbjdbiKtd8",
+        "dimensionItem": "EHbjdbiKtd8",
+        "displayName": "Number of malaria foci classified",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "IS9VxwhkJXR",
+        "dimensionItem": "IS9VxwhkJXR",
+        "displayName": "proportion of foci with response within N4 days of diagnosis",
+        "dimensionItemType": "INDICATOR"
+    }, {
+        "id": "lWrQhvx4YUA",
+        "dimensionItem": "lWrQhvx4YUA",
+        "displayName": "Proportion of cases investigated",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "c9o7AH6loTf",
+        "dimensionItem": "c9o7AH6loTf",
+        "displayName": "Proportion of cases classified",
+        "dimensionItemType": "INDICATOR"
+    }, {
+        "id": "gTPP0wYkXBA",
+        "dimensionItem": "gTPP0wYkXBA",
+        "displayName": "Total malaria confirmed cases",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "fj29hAOSNDk",
+        "dimensionItem": "fj29hAOSNDk",
+        "displayName": "Annual blood examination rate",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "HfEGwjpq3Rq",
+        "dimensionItem": "HfEGwjpq3Rq",
+        "displayName": "Presumed cases",
+        "dimensionItemType": "INDICATOR"
+    }, {
+        "id": "lZi1cjtvVB6",
+        "dimensionItem": "lZi1cjtvVB6",
+        "displayName": "Number of new active foci with the current calendar year",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "gTPP0wYkXBA",
+        "dimensionItem": "gTPP0wYkXBA",
+        "displayName": "Total malaria confirmed cases",
+        "dimensionItemType": "INDICATOR"
+    }, {
+        "id": "EM5cpI2NZ7d",
+        "dimensionItem": "EM5cpI2NZ7d",
+        "displayName": "Percentage of P. falciparum",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "clAcoaqWA5q",
+        "dimensionItem": "clAcoaqWA5q",
+        "displayName": "Percentage of P. vivax",
+        "dimensionItemType": "INDICATOR"
+    }, {
+        "id": "HMUc4RajgaC",
+        "dimensionItem": "HMUc4RajgaC",
+        "displayName": "Number of malaria cases classified as local",
+        "dimensionItemType": "DATA_ELEMENT"
+    }, {
+        "id": "kRasaq1REFp",
+        "dimensionItem": "kRasaq1REFp",
+        "displayName": "Number of total malaria cases (confirmed + presumed)",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "UHoPVgnALcd",
+        "dimensionItem": "UHoPVgnALcd",
+        "displayName": "Non-malaria outpatient cases",
+        "dimensionItemType": "INDICATOR"
+    }, {
+        "id": "GxlrIgMyEf4",
+        "dimensionItem": "GxlrIgMyEf4",
+        "displayName": "Number of malaria deaths",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "af7VIQU9Gsb",
+        "dimensionItem": "af7VIQU9Gsb",
+        "displayName": "Non-malaria inpatient deaths",
+        "dimensionItemType": "INDICATOR"
+    }, {
+        "id": "q5EPiDt5Xzt",
+        "dimensionItem": "q5EPiDt5Xzt",
+        "displayName": "Malaria inpatient reporting completeness (%)",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "YS1Rf9k88HX",
+        "dimensionItem": "YS1Rf9k88HX",
+        "displayName": "Malaria deaths reporting completeness (%)",
+        "dimensionItemType": "INDICATOR"
+    }, {
+        "id": "ZCCrHrs1Ta2",
+        "dimensionItem": "ZCCrHrs1Ta2",
+        "displayName": "Proportion of population at risk",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "gTPP0wYkXBA",
+        "dimensionItem": "gTPP0wYkXBA",
+        "displayName": "Total malaria confirmed cases",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "AvQDluaf46N",
+        "dimensionItem": "AvQDluaf46N",
+        "displayName": "Total malaria cases (confirmed+presumed)",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "IPI8Xmc1nnT",
+        "dimensionItem": "IPI8Xmc1nnT",
+        "displayName": "Test positivity rate (RDT)",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "fj29hAOSNDk",
+        "dimensionItem": "fj29hAOSNDk",
+        "displayName": "Annual blood examination rate",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "RqGYc77Xhek",
+        "dimensionItem": "RqGYc77Xhek",
+        "displayName": "% of suspected cases tested",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "v0WZQQ6gKAX",
+        "dimensionItem": "v0WZQQ6gKAX",
+        "displayName": "Number of inpatient malaria cases",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "GxlrIgMyEf4",
+        "dimensionItem": "GxlrIgMyEf4",
+        "displayName": "Number of malaria deaths",
+        "dimensionItemType": "DATA_ELEMENT"
+    }, {
+        "id": "ZCCrHrs1Ta2",
+        "name": "Proportion of population at risk",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "HxTrnX154BH",
+        "name": "No. of LLIN distributed",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "UH47dKFqTRK",
+        "name": "No. of LLIN distributed",
+        "dimensionItemType": "DATA_ELEMENT"
+    }, {
+        "id": "NywGy6uMS5r",
+        "name": "Number of people protected by IRS",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "UyuDDeXuIbr",
+        "name": "% population protected with IRS",
+        "dimensionItemType": "INDICATOR"
+    }, {
+        "id": "gTPP0wYkXBA",
+        "name": "Total malaria confirmed cases",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "SEi6VEB5DO0",
+        "name": "Annual parasite incidence(API)",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "IPI8Xmc1nnT",
+        "name": "Test positivity rate(TPR)",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "v0WZQQ6gKAX",
+        "name": "Number of inpatient malaria cases",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "GxlrIgMyEf4",
+        "name": "Number of malaria deaths",
+        "dimensionItemType": "DATA_ELEMENT"
+    }, {
+        "id": "PAbp5CFUJEP",
+        "name": "Domestic Funding % year",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "bMshElFlkIT",
+        "name": "External funding % year",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "LB63qxQWAot",
+        "name": "Funding gap % year",
+        "dimensionItemType": "DATA_ELEMENT"
+    }, {
+        "id": "ZCCrHrs1Ta2",
+        "dimensionItem": "ZCCrHrs1Ta2",
+        "displayName": "Proportion of population at risk",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "l7gcisIvTBN",
+        "dimensionItem": "l7gcisIvTBN",
+        "displayName": "No of ACT distributed to patients",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "M91K9z70dl4",
+        "dimensionItem": "M91K9z70dl4",
+        "displayName": "No. P.f cases treated with ACT",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "HFWOXsnDJo7",
+        "dimensionItem": "HFWOXsnDJo7",
+        "displayName": "Cases potentially treated with ACT(%)",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "iaQuylCoH3u",
+        "dimensionItem": "iaQuylCoH3u",
+        "displayName": "No of RDTs distributed",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "OMYp5Suv6P2",
+        "dimensionItem": "OMYp5Suv6P2",
+        "displayName": "RDT/ACT ration",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "UH47dKFqTRK",
+        "dimensionItem": "UH47dKFqTRK",
+        "displayName": "No. LLIN distributed",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "OcBiqoUWLYy",
+        "dimensionItem": "OcBiqoUWLYy",
+        "displayName": "% of population potentially covered with LLIN",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "NywGy6uMS5r",
+        "dimensionItem": "NywGy6uMS5r",
+        "displayName": "No of Population protected with IRS",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "UyuDDeXuIbr",
+        "dimensionItem": "UyuDDeXuIbr",
+        "displayName": "% of Population protected with IRS",
+        "dimensionItemType": "INDICATOR"
+    }, {
+        "id": "cOVhTyW8zN6",
+        "name": "Population at risk for malaria",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "X0luAFiy268",
+        "name": "Positive (micr+RDT)",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "DYpakDmCvNG",
+        "name": "Malaria classified as indigenous",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "Jlp3WR8fVnO",
+        "name": "Relapsing/recrudesance cases",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "Ppc51eaCPHb",
+        "name": "Cases investigated",
+        "dimensionItemType": "DATA_ELEMENT"
+    },
+    {
+        "id": "dASLnPMS8L8",
+        "name": "Prop. of cases investigated within 4 timeframe",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "HiH7BmkVTyK",
+        "name": "Number of P.Falciparum cases",
+        "dimensionItemType": "INDICATOR"
+    },
+    {
+        "id": "tGfdJPAcElX",
+        "name": "Number of P.Vivax Cases",
         "dimensionItemType": "INDICATOR"
     }
 ]
